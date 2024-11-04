c09e34762269dc44f9d3eae8f1055f2a9ccfbbc4
anandaalex <11221032@student.itk.ac.id>
2024-11-04 22:18:56 +0800

    perbaiki lebar navbar & hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,27 +12,28 @@ const Hero = () => {
       }}
     >
       <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
-        <h1 className="text-6xl font-bold text-white mb-4">
-          Selamat Datang di Tempat Kami
-        </h1>
-        <p className="font-normal text-xl text-white mb-6">
-          Nikmati pengalaman terbaik bersama kami.
-        </p>
-        <a
-          href="#reservasi"
-          className="bg-[rgba(24,50,98,0.8)] backdrop-blur-sm border border-[rgba(255,255,255,0.2)] text-white font-normal px-6 py-3 rounded-md hover:bg-[rgba(24,50,98,0.5)] transition duration-300"
-        >
-          Jelajahi Lebih Lanjut{" "}
-          <FontAwesomeIcon icon={faArrowDown} className="ml-2" />
-        </a>
-      </div>
-
-      <div className="absolute bottom-10 left-4 border rounded-full border-white text-white px-4 py-1 ml-16">
-        <p className="text-sm">
-          {" "}
-          <FontAwesomeIcon icon={faLocationDot} className="mr-1" /> Samboja,
-          Kutai Kartanegara
-        </p>
+        {/* Kontainer untuk konten Hero */}
+        <div className="max-w-7xl mx-auto px-4 w-full flex flex-col items-center text-center">
+          <h1 className="text-6xl font-bold text-white mb-4">
+            Selamat Datang di Tempat Kami
+          </h1>
+          <p className="font-normal text-xl text-white mb-6">
+            Nikmati pengalaman terbaik bersama kami.
+          </p>
+          <a
+            href="#reservasi"
+            className="bg-[rgba(24,50,98,0.8)] backdrop-blur-sm border border-[rgba(255,255,255,0.2)] text-white font-normal px-6 py-3 rounded-md hover:bg-[rgba(24,50,98,0.5)] transition duration-300"
+          >
+            Jelajahi Lebih Lanjut{" "}
+            <FontAwesomeIcon icon={faArrowDown} className="ml-2" />
+          </a>
+        </div>
+        <div className="absolute bottom-10 left-1 border rounded-full border-white text-white px-4 py-1 ml-32">
+          <p className="text-sm">
+            <FontAwesomeIcon icon={faLocationDot} className="mr-1" /> Samboja,
+            Kutai Kartanegara
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,135 +50,137 @@ const Navbar = () => {
           : "bg-transparent text-white"
       }`}
     >
-      {/* Logo */}
-      <div className="px-16" href="#beranda">
-        <img src={Logo} alt="/" className="w-24 h-auto" />
-      </div>
+      <div className="max-w-7xl mx-auto px-4 flex justify-between w-full">
+        {/* Logo */}
+        <div href="#beranda">
+          <img src={Logo} alt="/" className="w-24 h-auto" />
+        </div>
 
-      {/* Navigation Menu */}
-      <ul className="flex space-x-6 text-white font-semibold mx-4">
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              `hover:text-gray-200 ${isActive ? "font-bold" : "font-normal"}`
-            }
-          >
-            Beranda
-          </NavLink>
-        </li>
-        <li className="relative group">
-          <button className="flex items-center hover:text-blue-500 font-light transition-colors duration-300">
-            Fasilitas
-          </button>
-
-          {/* Dropdown Content */}
-          <ul className="absolute mt-2 w-52 bg-[rgba(24,50,98,0.5)] backdrop-blur-lg text-white shadow-lg rounded-lg py-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
-            <li className="px-6 py-3 hover:bg-blue-800 font-medium cursor-pointer border-b border-b-blue-400 last:border-b-0 rounded-lg transition-colors duration-200">
-              <a href="#kolam">🎣 Kolam Pancing</a>
-            </li>
-            <li className="px-6 py-3 hover:bg-blue-800 font-medium cursor-pointer border-b border-b-blue-400 last:border-b-0 rounded-lg transition-colors duration-200">
-              <a href="#resto">🍽️ Resto</a>
-            </li>
-            <li className="px-6 py-3 hover:bg-blue-800 font-medium cursor-pointer border-b border-b-blue-400 last:border-b-0 rounded-lg transition-colors duration-200">
-              <a href="#parkir">🚗 Parkiran</a>
-            </li>
-            <li className="px-6 py-3 hover:bg-blue-800 font-medium cursor-pointer rounded-lg transition-colors duration-200">
-              <a href="#sewa">🎟️ Penyewaan</a>
-            </li>
-          </ul>
-        </li>
-
-        <li>
-          <NavLink
-            to="/lomba"
-            className={({ isActive }) =>
-              `hover:text-gray-200 ${isActive ? "font-bold" : "font-normal"}`
-            }
-          >
-            Lomba
-          </NavLink>
-        </li>
-        <li>
-          <a
-            href="#kontak"
-            className={`hover:text-gray-200 ${
-              activeMenu === "kontak" ? "font-bold" : "font-normal"
-            }`}
-            onClick={() => handleMenuClick("kontak")}
-          >
-            Kontak
-          </a>
-        </li>
-        <li>
-          <a
-            href="#reservasi"
-            className={` text-white font-light px-4 py-2 rounded-3xl  transition duration-300 ${
-              isScrolled
-                ? "bg-white hover:bg-gray-300 text-[rgba(24,50,98,1.0)]"
-                : "bg-[rgba(24,50,98,0.8)] hover:bg-[rgba(24,50,98,0.5)]"
-            }`}
-          >
-            Reservasi
-          </a>
-        </li>
-      </ul>
-
-      {/* Language Dropdown */}
-      <div className="relative group px-16">
-        <button
-          onClick={toggleLanguageDropdown}
-          className="flex items-center text-white font-semibold"
-        >
-          <FontAwesomeIcon icon={faGlobe} className="mr-2" />
-          {language === "en" ? (
-            <img
-              src="https://flagcdn.com/w20/gb.png"
-              alt="UK flag"
-              className="w-5 h-auto mr-1"
-            />
-          ) : (
-            <img
-              src="https://flagcdn.com/w20/id.png"
-              alt="Indonesian flag"
-              className="w-5 h-auto mr-1"
-            />
-          )}
-          <FontAwesomeIcon
-            icon={faChevronDown}
-            className={`ml-1 transition-transform duration-300 ${
-              isLanguageOpen ? "rotate-180" : ""
-            }`}
-          />
-        </button>
-
-        {/* Dropdown Menu */}
-        {isLanguageOpen && (
-          <ul className="absolute text-white right-0 mt-2 bg-[rgba(24,50,98,0.2)] backdrop-blur-lg shadow-lg rounded-lg">
-            <li
-              onClick={() => handleLanguageChange("en")}
-              className="px-4 py-2 hover:bg-[rgba(24,50,98,0.8)] cursor-pointer border-b border-b-blue-400 last:border-b-0 rounded-lg flex items-center"
+        {/* Navigation Menu */}
+        <ul className="flex space-x-6 text-white font-semibold mx-4">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `hover:text-gray-200 ${isActive ? "font-bold" : "font-normal"}`
+              }
             >
+              Beranda
+            </NavLink>
+          </li>
+          <li className="relative group">
+            <button className="flex items-center hover:text-blue-500 font-light transition-colors duration-300">
+              Fasilitas
+            </button>
+
+            {/* Dropdown Content */}
+            <ul className="absolute mt-2 w-52 bg-[rgba(24,50,98,0.5)] backdrop-blur-lg text-white shadow-lg rounded-lg py-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
+              <li className="px-6 py-3 hover:bg-blue-800 font-medium cursor-pointer border-b border-b-blue-400 last:border-b-0 rounded-lg transition-colors duration-200">
+                <a href="#kolam">🎣 Kolam Pancing</a>
+              </li>
+              <li className="px-6 py-3 hover:bg-blue-800 font-medium cursor-pointer border-b border-b-blue-400 last:border-b-0 rounded-lg transition-colors duration-200">
+                <a href="#resto">🍽️ Resto</a>
+              </li>
+              <li className="px-6 py-3 hover:bg-blue-800 font-medium cursor-pointer border-b border-b-blue-400 last:border-b-0 rounded-lg transition-colors duration-200">
+                <a href="#parkir">🚗 Parkiran</a>
+              </li>
+              <li className="px-6 py-3 hover:bg-blue-800 font-medium cursor-pointer rounded-lg transition-colors duration-200">
+                <a href="#sewa">🎟️ Penyewaan</a>
+              </li>
+            </ul>
+          </li>
+
+          <li>
+            <NavLink
+              to="/lomba"
+              className={({ isActive }) =>
+                `hover:text-gray-200 ${isActive ? "font-bold" : "font-normal"}`
+              }
+            >
+              Lomba
+            </NavLink>
+          </li>
+          <li>
+            <a
+              href="#kontak"
+              className={`hover:text-gray-200 ${
+                activeMenu === "kontak" ? "font-bold" : "font-normal"
+              }`}
+              onClick={() => handleMenuClick("kontak")}
+            >
+              Kontak
+            </a>
+          </li>
+          <li>
+            <a
+              href="#reservasi"
+              className={` text-white font-light px-4 py-2 rounded-3xl  transition duration-300 ${
+                isScrolled
+                  ? "bg-white hover:bg-gray-300 text-[rgba(24,50,98,1.0)]"
+                  : "bg-[rgba(24,50,98,0.8)] hover:bg-[rgba(24,50,98,0.5)]"
+              }`}
+            >
+              Reservasi
+            </a>
+          </li>
+        </ul>
+
+        {/* Language Dropdown */}
+        <div className="relative group">
+          <button
+            onClick={toggleLanguageDropdown}
+            className="flex items-center text-white font-semibold"
+          >
+            <FontAwesomeIcon icon={faGlobe} className="mr-2" />
+            {language === "en" ? (
               <img
                 src="https://flagcdn.com/w20/gb.png"
-                alt="US flag"
-                className="w-5 h-auto mr-2"
+                alt="UK flag"
+                className="w-5 h-auto mr-1"
               />
-              English
-            </li>
-            <li
-              onClick={() => handleLanguageChange("id")}
-              className="px-4 py-2 hover:bg-[rgba(24,50,98,0.8)] cursor-pointer border-b border-b-blue-400 last:border-b-0 rounded-lg flex items-center"
-            >
+            ) : (
               <img
                 src="https://flagcdn.com/w20/id.png"
                 alt="Indonesian flag"
-                className="w-5 h-auto mr-2"
+                className="w-5 h-auto mr-1"
               />
-              Indonesia
-            </li>
-          </ul>
-        )}
+            )}
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              className={`ml-1 transition-transform duration-300 ${
+                isLanguageOpen ? "rotate-180" : ""
+              }`}
+            />
+          </button>
+
+          {/* Dropdown Menu */}
+          {isLanguageOpen && (
+            <ul className="absolute text-white right-0 mt-2 bg-[rgba(24,50,98,0.2)] backdrop-blur-lg shadow-lg rounded-lg">
+              <li
+                onClick={() => handleLanguageChange("en")}
+                className="px-4 py-2 hover:bg-[rgba(24,50,98,0.8)] cursor-pointer border-b border-b-blue-400 last:border-b-0 rounded-lg flex items-center"
+              >
+                <img
+                  src="https://flagcdn.com/w20/gb.png"
+                  alt="US flag"
+                  className="w-5 h-auto mr-2"
+                />
+                English
+              </li>
+              <li
+                onClick={() => handleLanguageChange("id")}
+                className="px-4 py-2 hover:bg-[rgba(24,50,98,0.8)] cursor-pointer border-b border-b-blue-400 last:border-b-0 rounded-lg flex items-center"
+              >
+                <img
+                  src="https://flagcdn.com/w20/id.png"
+                  alt="Indonesian flag"
+                  className="w-5 h-auto mr-2"
+                />
+                Indonesia
+              </li>
+            </ul>
+          )}
+        </div>
       </div>
     </nav>
   );
